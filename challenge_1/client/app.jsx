ddba31532b064b6b636cc76631ce802fa794e6dd
elmarek <mareks@gmx.com>
2020-07-29 14:45:26 -0700

    Completed Mini App

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {}
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [],
+      isLoaded: false,
+      value: "",
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
   }
 
+  handleSubmit(e) {
+    e.preventDefault(e);
+    fetch(`/events?q=${this.state.value}&_page&_limit=20`)
+      .then((data) => data.json())
+      .then((events) => {
+        this.setState({
+          events: events,
+          isLoaded: true,
+        });
+      })
+      .catch((err) => console.log("error getting events: ", err));
+  }
 
   render() {
-    return (
-    <div>
-    <p>Hello WORLD!</p>
-    </div>
-    )
+    if (this.state.isLoaded) {
+      return (
+        <div>
+          {this.state.events.map((event) => {
+            return <div key={event.description}>{event.description}</div>;
+          })}
+        </div>
+      );
+    } else {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <p>Search Event: </p>
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
   }
 }
 
-
-
-ReactDOM.render( <App />, document.getElementById('app')
-);
-
+ReactDOM.render(<App />, document.getElementById("app"));
